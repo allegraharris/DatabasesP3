7a115ce8098a8efc7eb80872159e29771f376b53
Charliemei1112 <jm3438@georgetown.edu>
2023-11-18 12:03:42 -0500

    Create, show, describe

--- main.py
+++ main.py
@@ -1,21 +1,64 @@
-# Create, show, describe
-# Assume one input at each time
+# DatabasesP3 Design Ideas
+### **Assumption 1**: Only one valid and complete input is taken at one time
+### **Assumption 2**: All of the key words are case insensitive, but everything else is sensitive
+### **Assumption 3**: Assume we only need primary indexing structure for single key attribute
+### **table definition**: 
+#   table = [ columns, column, indexing, primary_key, tuple_1, tuple_2, ......, tuple_n ]
+#   columns = { col_1:[<type>,<primary_key[yes:1,no:0]>,<index>], col_2, ......., col_n }
+#   column = [ col_1, col_2, ......, col_n]
+#   primary = { primary_key_1, primary_key_2, ......, primary_key_n }
+#   indexing = { key_1:tuple_1, key_2:tuple_2, ......., key_n:tuple_n }
+#   tuple_1 = [ col_1.value, col_2.value, ......, col_n.value ], tuple_2, ......, tuple_n
+### **
 
 import sqlparse
 import time
+from BTrees._OOBTree import OOBTree
 # from test import eval_query
 # import test
 # import getch
 
-SQLCommand = ['CREATE','SHOW','DESCRIBE'] # keyWord is not case sensitive
-Datatype = ['INT','STRING']
-sql_query = "CREATE TABLE people { key_no INT NOT NULL, first_name INT NOT NULL, last_name INT NOT NULL, primary_key (key_no)};"
+keywords = ['CREATE','SHOW','DESCRIBE','INSERT','INTO','TABLE','TABLES','INT','STRING','PRIMARY','FOREIGN','KEY']
+sqlCommand = ['CREATE','SHOW','DESCRIBE','INSERT'] # keyWord is not case sensitive
+datatype = ['INT','STRING']
+key = ['PRIMARY','FOREIGN','KEY']
+sql_query = "CREATE TABLE people ( key_no INT NOT NULL, first_name INT NOT NULL, last_name INT NOT NULL, primary_key (key_no) );"
 query_tokens = []
 quitting = False 
 PROMPT = "-> "
 PROMPT2 = "> "
 
-tables = {}
+databases = {}
+describe_columns = { 'Field':['STRING',0], 'Type':['STRING',0], 'Primary':['STRING',0]} # describe
+# Column 
+
+# general syntax error
+class Syntax_Error(Exception):
+    def __init__(self, message="Syntax Error"):
+        self.message = message
+        super().__init__(self.message)
+
+# Error if non-keyword use preserved keyword
+class Keyword_Used(Exception):
+    def __init__(self, message="Keyword_Used"):
+        self.message = message
+        super().__init__(self.message)
+
+class Invalid_Type(Exception):
+    def __init__(self, message="Invalid Data Type"):
+        self.message = message
+        super().__init__(self.message)
+
+# Error if item is repeated. Such as repeated primary key, repeated column, etc.
+class Duplicate_Item(Exception):
+    def __init__(self, message="Duplicate Item"):
+        self.message = message
+        super().__init__(self.message)
+
+class TABLE_EXIST(Exception):
+    def __init__(self, message="Table not exist"):
+        self.message = message
+        super().__init__(self.message)
 
 # Begin Parsing Functions
 def readInput():
@@ -56,28 +99,182 @@ def filter():
 
 # End of parsing functions
 
+# some special function
+# check if user input is keyword
+def is_keyword(word):
+    for str in keywords:
+        if word == str:
+            return True
+    return False
+
+
 ## Begin evaluation functions
 def find_operation():
     try:
-        return SQLCommand.index(query_tokens[0])
+        return sqlCommand.index(query_tokens[0])
     except ValueError:
         return -1
-        
+    
+def show_table():
+    table = [{'Tables':['STRING',0]},{},[],[]]
+    for table_name in databases.keys():
+        list = [table_name]
+        table.append(list)
+    return table
+
+def create_table():
+    global databases
+    table_name = query_tokens[2]
+    table = [{},[],[],{}]
+    table = parse_columns(table)
+    databases[table_name] = table
+    return ["Query OK, 0 rows affected"]
+
+def parse_columns(table):
+    parsed = query_tokens[3]
+    if parsed[0] != '(' or parsed[len(parsed)-1] != ')':
+        raise Syntax_Error("Syntax Error: attributes is invalid")
+    if len(parsed) == 2 or parsed[1:len(parsed)-1].isspace():
+        raise Syntax_Error("Syntax Error: attributes cannot be empty")
+    attributes = parsed[1:len(parsed)-1].strip().split(',')
+    # columns = table[0]
+    # column = table[1]
+    # keys = table[2]
+    index = 0
+    for attribute in attributes:
+        tokens = attribute.strip().split(' ')
+        print(tokens)
+        name = tokens[0]
+        type = tokens[1].upper()
+        if name == 'PRIMARY':
+            if len(tokens) != 3 or tokens[1] != 'KEY':
+                raise Syntax_Error("Syntax Error: Primary key")
+            keys = tokens[2][1:len(tokens[2])-1].strip().split(',')
+            for key in keys:
+                if key not in table[0]:
+                    raise Syntax_Error("Syntax Error: Column '" + key + "' does not exist")
+                table[0][key][1] = 1 
+                table[2].append(key)
+        if name == 'FOREIGN':
+            if len(tokens) != 3 or tokens[1] != 'KEY':
+                raise Syntax_Error("Syntax Error: Primary key")
+            key = tokens[2][1:len(tokens[2])-1].strip()
+            if key not in table[0]:
+                raise Syntax_Error("Syntax Error: Column '" + key + "' does not exist")
+            table[0][key][1] = 2
+        if name != 'PRIMARY':
+            if name in table[0]:
+                raise Duplicate_Item("Duplicate column name " + tokens[0])
+            if type not in datatype:
+                raise Invalid_Type("Invalid Data type '" + tokens[1] + "'")
+            table[0][name] = [type,0,index]
+            index = index+1
+            table[1].append(name)   
+    return table
+
+
+def describe_table(table_name):
+    my_table = [describe_columns,{},[],[]]
+    table = databases[table_name]
+    columns = table[0]
+    for column in columns.keys():
+        my_table.append([column,columns[column][0],columns[column][1]])
+    return my_table
+
+def insert(table_name):
+    num = 0
+    return [f"Query OK, {num} rows affected"]
+    
 
 def eval_query():
     optr = find_operation()
-    print(optr)
-    return
+    # print(optr)
+    if optr == 0:
+        if query_tokens[1] != 'TABLE' or len(query_tokens) != 5:
+            raise Syntax_Error("Syntax Error: TABLE")
+        table_name = query_tokens[2]
+        if is_keyword(table_name):
+            raise Keyword_Used("Keyword used:" + query_tokens[2])
+        if table_name in databases:
+            raise TABLE_EXIST("Table '" + table_name + "' exists")
+        return create_table()
+    if optr == 1:
+        if len(query_tokens) != 3 or query_tokens[1] != 'TABLES':
+            raise Syntax_Error("Syntax Error: SHOW TABLES")
+        return show_table()
+    if optr == 2:
+        if len(query_tokens) != 3:
+            raise Syntax_Error("Syntax Error: Describe")
+        if query_tokens[1] not in databases:
+            raise TABLE_EXIST("Table '" + query_tokens[1] + "' does not exist")
+        return describe_table(query_tokens[1])
+    if optr == 3:
+        if query_tokens[1] != 'INTO' or len(query_tokens) != 5:
+            raise Syntax_Error("Syntax Error: INSERT")
+        insert_info = query_tokens[2].split(' ')
+        table_name = insert_info[0]
+        if table_name not in databases:
+            raise TABLE_EXIST("Table '" + insert_info[0] + "' does not exist")
+        column = insert_info[1]
+        indexing = []
+        if len(column) == 2 or column[1:len(column)-1].strip().isspace():
+            return insert(table_name,indexing)
+        attributes = column[1:len(column)-1].strip().split(',')
+        for attribute in attributes:
+            if attribute not in databases[table_name][0]:
+                raise TABLE_EXIST(f"Column {attribute} does not exist")
+            indexing.append
+    raise Syntax_Error("Unknown SQL Command")
+
+# R is our relations
+def print_table(R):
+    if len(R) == 1: # not relation output (create, insert)
+        print(R[0])
+        return
+    if len(R) == 4:
+        print("Empty set")
+        return
+    message = []
+    title = ""
+    for key in R[0].keys():
+        title += f"{key:20}"
+    message.append(title)
+    for i in range (4,len(R)):
+        tuple = ""
+        for attribute in R[i]:
+            tuple += f"{attribute:20}"
+        message.append(tuple)
+    for line in message:
+        print(line)
+    return 
 
 
 while quitting == False:
-    readInput()
-    filter()
-    print(sql_query)
-    print(query_tokens)
-    if query_tokens[0] == "quit":
-        break
-    eval_query()
+    try:
+        readInput()
+        filter()
+        # print(sql_query)
+        print(query_tokens)
+        if query_tokens[0] == "quit":
+            break
+        start_time = time.time()
+        print_table(eval_query())
+        end_time = time.time()
+        print(f"Time: {end_time-start_time:.3f}s")
+        # print(databases)
+    # throw errors
+    except Syntax_Error as e:
+        print(f"{e}")
+    except Keyword_Used as e:
+        print(f"{e}")
+    except Invalid_Type as e:
+        print(f"{e}")
+    except Duplicate_Item as e:
+        print(f"{e}")
+    except TABLE_EXIST as e:
+        print(f"{e}")
+# for table in databases.keys():
+#     print_table(databases[table])
     
 
 
--- test.py
+++ test.py
@@ -1,17 +1,30 @@
+# # database = {}
+# # table = {}
+# # columns = {'key_no':('INT',1),'first_name':('STRING',0),'last_name':('STRING',0)} # 1: indicate primary key
+# # # type = ['INT','STRING','STRING']
+# # item1 = (1,'Charlie','Mei')
+# # item2 = (2,'Andy','Han')
+# # table = [columns]
+# # print(columns['key_no'][1])
+# # print(table[0]['key_no'][0])
+# # table.append(item1)
+# # print(table[1])
+# # database['table1'] = table
+# # # database.append(table)
+# # print(database)
+
 database = {}
-table = {}
-columns = {'key_no':('INT',1),'first_name':('STRING',0),'last_name':('STRING',0)} # 1: indicate primary key
-# type = ['INT','STRING','STRING']
-item1 = (1,'Charlie','Mei')
-item2 = (2,'Andy','Han')
-table = [columns]
-print(columns['key_no'][1])
-print(table[0]['key_no'][0])
-table.append(item1)
-print(table[1])
+table = []
 database['table1'] = table
-# database.append(table)
-print(database)
+columns = {'key_no':('INT',1),'first_name':('STRING',0),'last_name':('STRING',0)} 
+item1 = (1,'Charlie','Mei')
+table.append(columns)
+table.append(item1)
+for key in table[0].keys():
+    print(key)
+# select * from table1
+# return database['table1']
+# select first_name from table
+# output first_name':('STRING',0)
 
-
-
+# # research on BTree
