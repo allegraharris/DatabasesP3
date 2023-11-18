fa5f1f83d117ce0c03689d997a36d38e84587897
Charliemei1112 <jm3438@georgetown.edu>
2023-11-18 15:25:49 -0500

    Create Insert Show describe

--- test.py
+++ test.py
@@ -13,18 +13,49 @@
 # # # database.append(table)
 # # print(database)
 
-database = {}
-table = []
-database['table1'] = table
-columns = {'key_no':('INT',1),'first_name':('STRING',0),'last_name':('STRING',0)} 
-item1 = (1,'Charlie','Mei')
-table.append(columns)
-table.append(item1)
-for key in table[0].keys():
-    print(key)
+# database = {}
+# table = []
+# database['table1'] = table
+# columns = {'key_no':('INT',1),'first_name':('STRING',0),'last_name':('STRING',0)} 
+# item1 = (1,'Charlie','Mei')
+# table.append(columns)
+# table.append(item1)
+# for key in table[0].keys():
+#     print(key)
 # select * from table1
 # return database['table1']
 # select first_name from table
 # output first_name':('STRING',0)
 
 # # research on BTree
+import re
+
+# input_string = "(1,2,3), (2,3,4), (2,4,5)"
+
+# # Use regular expression to split by commas outside parentheses
+# parsed_strings = re.split(r',\s*(?![^()]*\))', input_string)
+
+# # Remove leading and trailing whitespaces from each parsed string
+# parsed_strings = [s.strip() for s in parsed_strings]
+
+# print(parsed_strings)
+
+# tuple1 = {1,2,'efew'}
+# tuple2 = {2,1,'efew'}
+# if tuple1 == tuple2:
+#     print("they are same")
+# import re
+
+# input_string = "one, 'two, three', four, 'five, six', seven"
+
+# # Use regular expression to split by commas outside quotes
+# parsed_strings = re.split(r',\s*(?=(?:[^\'"]*[\'"][^\'"]*[\'"])*[^\'"]*$)', input_string)
+
+# # Remove leading and trailing single or double quotes
+# parsed_strings = [re.sub(r'^[\'"]|[\'"]$', '', s) for s in parsed_strings]
+
+# print(parsed_strings)
+tuple1 = tuple({1, 2, 3})
+tuple2 = tuple({2,3,4})
+if tuple1 == tuple2:
+    print("they are same")
