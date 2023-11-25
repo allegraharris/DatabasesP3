743bddc533b26adb7c29a62418619fcceb509dee
sarahg0201 <smg329@georgetown.edu>
2023-11-25 13:43:24 -0500

    preloaded relations and test queries

---
+++ create.sql
@@ -0,0 +1,23 @@
+CREATE TABLE rel1 (
+    colOne INT, 
+    colA INT, 
+    primary key (colOne)
+);
+
+CREATE TABLE rel2 (
+	colOne INT, 
+	colB INT, 
+	primary key (colOne)
+);
+
+CREATE TABLE rel3 (
+	colOne INT, 
+	colC INT, 
+	primary key (colOne)
+);
+
+CREATE TABLE rel4 (
+	colOne INT, 
+	colD INT, 
+	primary key (colOne)
+);
