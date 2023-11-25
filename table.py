ca359a2f9239d4e9790cfb9287a62a436f7d0454
Charliemei1112 <jm3438@georgetown.edu>
2023-11-25 12:56:59 -0500

    Fix digits in column names

--- table.py
+++ table.py
@@ -29,7 +29,8 @@ class Table:
     ### add column, primary key, or foreign key ###
     ### attribute is the raw input, e.g: net_id INT, PRIMARY KEY (net_id), PRIMARY KEY ()
     def add_attribute(self,attribute):
-        tokens = [token for token in re.split(r'\s+|([a-zA-Z_]+)|(\([^)]+\))',attribute) if token]
+        tokens = [token for token in re.split(r'\s(?![^()]*\))',attribute) if token]
+        # print(tokens)
         if tokens[0] == 'PRIMARY': # primary key definition
             if len(tokens) != 3 or tokens[1].upper() != 'KEY':
                 raise Syntax_Error("Syntax Error: Primary Key")
