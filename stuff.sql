6c301a1c144ca8510da2a2a675e14c4a66695503
sarahg0201 <smg329@georgetown.edu>
2023-11-27 23:43:34 -0500

    test files

--- stuff.sql
+++ stuff.sql
@@ -8,13 +8,13 @@ SELECT * FROM employees WHERE emp_id >= 2370;
 
 SELECT * FROM departments WHERE dep_name = 'css' OR dep_name = 'physics';
 
-SELECT * FROM employees JOIN departments ON employees.dep_id = departments.dep_id;
+SELECT employees.emp_id, employees.dep_id, employees.emp_name, departments.dep_name FROM employees JOIN departments ON employees.dep_id = departments.dep_id;
 
-SELECT * FROM employees JOIN departments ON employees.dep_id = departments.dep_id WHERE employees.dep_id=3;
+SELECT employees.emp_id, employees.dep_id, employees.emp_name, departments.dep_name FROM employees JOIN departments ON employees.dep_id = departments.dep_id WHERE employees.dep_id=3;
 
 SELECT employees.emp_id, employees.dep_id, employees.emp_name FROM employees JOIN departments ON employees.dep_id = departments.dep_id WHERE employees.dep_id=3 AND employees.emp_name='charlie mei';
 
-SELECT employees.emp_id, employees.dep_id, employees.emp_name FROM employees JOIN departments ON employees.dep_id = departments.dep_id WHERE employees.emp_name='sarah green' OR departments.dep_name= 'cs';
+SELECT employees.emp_id, departments.dep_id, employees.emp_name, departments.dep_name FROM employees JOIN departments ON employees.dep_id = departments.dep_id WHERE employees.emp_name='sarah green' OR departments.dep_name= 'cs';
 
 SELECT * FROM employees JOIN departments ON employees.dep_id = departments.dep_id WHERE departments.dep_name != 'biology' AND employees.emp_id >= 1499;
 
