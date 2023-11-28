785c5974a08b2405837b60ff4deefcd62acf850d
sarahg0201 <smg329@georgetown.edu>
2023-11-27 21:45:36 -0500

    file

---
+++ stuff.sql
@@ -0,0 +1,21 @@
+SELECT * FROM departments;
+
+SELECT * FROM employees;
+
+SELECT max(emp_id) FROM employees;
+
+SELECT * FROM employees WHERE emp_id >= 2370;
+
+SELECT * FROM departments WHERE dep_name = ‘css’ OR dep_name = 'physics';
+
+SELECT * FROM employees JOIN departments ON employees.dep_id = departments.dep_id;
+
+SELECT * FROM employees JOIN departments ON employees.dep_id = departments.dep_id WHERE employees.dep_id=3;
+
+SELECT * FROM employees JOIN departments ON employees.dep_id = departments.dep_id WHERE employees.dep_id=3 AND employees.emp_name='charlie mei';
+
+SELECT * FROM employees JOIN departments ON employees.dep_id = departments.dep_id WHERE employees.emp_name='sarah green' OR departments.dep_name= 'cs';
+
+SELECT * FROM employees JOIN departments ON employees.dep_id = departments.dep_id WHERE departments.dep_name != 'biology' AND employees.emp_id >= 1499;
+
+SELECT * FROM employees JOIN departments ON employees.dep_id = departments.dep_id WHERE employees.dep_id != departments.dep_id;
